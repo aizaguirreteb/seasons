cbb710cf546c9fae4ac6550146f6660bbf5ed8f0
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-29 16:50:44 +0200

    Refactoring to class component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+// const App = () => {
 
-    /**
-     * Geolocation API
-     */
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    )
+//     /**
+//      * Geolocation API
+//      */
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     )
 
 
-    return <div>Hi there!</div>;
+//     return <div>Latitude: </div>;
+// };
+
+class App extends React.Component {
+
+    render () {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        )
+        return <div>Latitude: </div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));

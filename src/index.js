361a9f1a29d01bb949dda7354fa2fededb4315ce
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-29 20:02:07 +0200

    Rendering component depending on property

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 // const App = () => {
 
@@ -47,11 +48,7 @@ class App extends React.Component {
         }
 
         if( this.state.lat && !this.state.errorMessage ){
-            return (
-                <div>
-                    Latitude: {this.state.lat}
-                </div>
-            )
+            return <SeasonDisplay lat={this.state.lat} />    
         }
 
         return (

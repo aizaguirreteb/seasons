f8f2cd63ad79bfbd23e4935812b528ce4d958e8f
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-29 17:22:55 +0200

    Rerendering depending on condition

--- src/index.js
+++ src/index.js
@@ -21,19 +21,37 @@ class App extends React.Component {
         super(props);
 
         //This is our state object!
-        this.state = { lat: null }
+        this.state = { lat: null, errorMessage: '' }
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({ lat: position.coords.latitude })
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
         )
     }
 
     render () {
+
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if( this.state.lat && !this.state.errorMessage ){
+            return (
+                <div>
+                    Latitude: {this.state.lat}
+                </div>
+            )
+        }
+
+        return (
+            <div>Loading...</div>
+        )
         
-        return <div>Latitude: {this.state.lat}</div>
+        
     }
 }
 

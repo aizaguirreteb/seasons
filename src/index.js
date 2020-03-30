59a8ad53fb6a57f2dad1a38fcdd96a8386492a03
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-03-30 08:55:43 +0200

    Refactoring to avoid multiple returns on render

--- src/index.js
+++ src/index.js
@@ -42,8 +42,7 @@ class App extends React.Component {
         console.log('My component updated itself')
     }
 
-    render () {
-
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -53,8 +52,14 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept location request"/>
-        
-        
+    }
+
+    render () {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
